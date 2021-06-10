537684db2e8be7e6336eaa321be99963277763f3
shekhutsav1962001 <shekhutsav1962001@gmail.com>
2021-06-10 10:38:41 +0530

    url added

--- server.js
+++ server.js
@@ -194,7 +194,7 @@ app.get("/currentsong", (_, res) => {
                                   
                                               <div class="ant-list-item-meta-content">
                                                   <h4 class="ant-list-item-meta-title">
-                                                      <a class="ellipsis-overflow a-spotify" target="_blank" rel="noopener noreferrer"  title="${name}">${name}</a>
+                                                      <a href="${url}"  class="ellipsis-overflow a-spotify" target="_blank" rel="noopener noreferrer"  title="${name}">${name}</a>
                                                   </h4>
                                                   
                                             </div>
